e0c466dc2a695052e7c5939eecf4a9b04fec6731
StefanDob <stefan.dobrea@stud.tu-darmstadt.de>
2024-05-07 18:51:44 +0200

    first draft of adding acount feature added in frontend

--- src/main/java/de/tu/darmstadt/frontend/MainLayout.java
+++ src/main/java/de/tu/darmstadt/frontend/MainLayout.java
@@ -2,10 +2,13 @@ package de.tu.darmstadt.frontend;
 
 import com.vaadin.flow.component.applayout.AppLayout;
 import com.vaadin.flow.component.applayout.DrawerToggle;
+import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.html.Footer;
 import com.vaadin.flow.component.html.H1;
 import com.vaadin.flow.component.html.H2;
 import com.vaadin.flow.component.html.Header;
+import com.vaadin.flow.component.orderedlayout.FlexComponent;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.Scroller;
 import com.vaadin.flow.component.sidenav.SideNav;
 import com.vaadin.flow.component.sidenav.SideNavItem;
@@ -13,6 +16,7 @@ import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.theme.lumo.LumoUtility;
 import de.tu.darmstadt.frontend.account.AccountView;
 import de.tu.darmstadt.frontend.dashboard.DashboardView;
+import de.tu.darmstadt.frontend.store.LoginDialog;
 import de.tu.darmstadt.frontend.store.StoreView;
 import de.tu.darmstadt.frontend.warenkorb.WarenkorbView;
 import org.vaadin.lineawesome.LineAwesomeIcon;
@@ -23,6 +27,8 @@ import org.vaadin.lineawesome.LineAwesomeIcon;
 public class MainLayout extends AppLayout {
 
     private H2 viewTitle;
+
+    private Button loginButton;
 
     public MainLayout() {
         setPrimarySection(Section.DRAWER);
@@ -37,7 +43,23 @@ public class MainLayout extends AppLayout {
         viewTitle = new H2();
         viewTitle.addClassNames(LumoUtility.FontSize.LARGE, LumoUtility.Margin.NONE);
 
-        addToNavbar(true, toggle, viewTitle);
+        // Create a layout for the login button
+        Button loginButton = new Button("Login");
+        loginButton.addClickListener(e -> {
+            createAccount();
+        });
+        loginButton.getStyle().set("margin-right", "16px"); // Adjust margin as needed
+
+        HorizontalLayout headerRightContent = new HorizontalLayout();
+        headerRightContent.add(loginButton);
+        headerRightContent.setWidthFull();
+        headerRightContent.setJustifyContentMode(FlexComponent.JustifyContentMode.END);
+        addToNavbar(true, toggle, viewTitle, headerRightContent);
+    }
+
+    private void createAccount() {
+        LoginDialog accountDialog = new LoginDialog();
+        accountDialog.open();
     }
 
     private void addDrawerContent() {
