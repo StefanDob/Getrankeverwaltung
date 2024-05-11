aa1dc095693f40b2f6e9d8ba9b2c49da4c63799b
StefanDob <stefan.dobrea@stud.tu-darmstadt.de>
2024-05-11 14:53:28 +0200

    slight update of login dialog desing

--- src/main/java/de/tu/darmstadt/frontend/store/LoginDialog.java
+++ src/main/java/de/tu/darmstadt/frontend/store/LoginDialog.java
@@ -4,7 +4,12 @@ import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.dialog.Dialog;
 import com.vaadin.flow.component.formlayout.FormLayout;
 import com.vaadin.flow.component.html.H2;
+import com.vaadin.flow.component.html.Header;
+import com.vaadin.flow.component.icon.Icon;
+import com.vaadin.flow.component.icon.VaadinIcon;
 import com.vaadin.flow.component.notification.Notification;
+import com.vaadin.flow.component.orderedlayout.FlexComponent;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.textfield.PasswordField;
 import com.vaadin.flow.component.textfield.TextField;
 import de.tu.darmstadt.backend.backendService.AccountOperations;
@@ -15,8 +20,22 @@ public class LoginDialog extends Dialog {
     private PasswordField passwordField;
 
     public LoginDialog() {
-        // Create header with title
-        H2 title = new H2("Login");
+        Header header = new Header(new H2("Login"));
+
+        HorizontalLayout headerLayout = new HorizontalLayout();
+        headerLayout.setWidthFull();
+        headerLayout.setJustifyContentMode(FlexComponent.JustifyContentMode.BETWEEN);
+        headerLayout.setAlignItems(FlexComponent.Alignment.BASELINE);
+
+
+        Button closeButton = new Button(new Icon(VaadinIcon.CLOSE));
+        closeButton.addClickListener(e -> {
+            close(); // Close the dialog without performing any action
+        });
+
+        // Add title to the left of the header layout
+        headerLayout.add(header, closeButton);
+
 
         // Create form layout for fields
         FormLayout formLayout = new FormLayout();
@@ -58,14 +77,11 @@ public class LoginDialog extends Dialog {
 
         });
 
-        Button cancelButton = new Button("Cancel");
-        cancelButton.setWidthFull();
-        cancelButton.addClickListener(e -> {
-            close(); // Close the dialog without performing any action
-        });
 
-        formLayout.add(usernameField, passwordField, loginButton, registerButton, cancelButton);
-        add(formLayout);
+
+
+        formLayout.add(usernameField, passwordField, loginButton, registerButton);
+        add(headerLayout, formLayout);
     }
 
     private void loginUser() {
