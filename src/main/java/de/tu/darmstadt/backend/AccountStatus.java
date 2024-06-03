f9537fa7555512f4ffb946f9b44c1229e44452d1
Toni Tran <tan_phat.tran@stud.tu-darmstadt.de>
2024-06-03 23:10:07 +0200

    Constructor added to the enum 'AccountStatus'

--- src/main/java/de/tu/darmstadt/backend/AccountStatus.java
+++ src/main/java/de/tu/darmstadt/backend/AccountStatus.java
@@ -7,23 +7,40 @@ public enum AccountStatus {
     /**
      * The default status of an {@link Account}.
      */
-    STANDARD,
+    STANDARD("Standard"),
 
     /**
      * A privileged {@link Account} is allowed to alter the {@link AccountStatus} of other {@link AccountStatus} and
      * to perform privileged tasks.
      */
-    PRIVILEGED,
+    PRIVILEGED("Privileged"),
 
     /**
      * An {@link Account} is restricted if a condition required by an ice cream shop is not met yet.
      */
-    RESTRICTED,
+    RESTRICTED("Restricted"),
 
     /**
      * A closed {@link Account} cannot be logged in anymore, e.g., due to terms of use violations.
      */
-    CLOSED
+    CLOSED("Closed");
+
+    /**
+     * This attribute stores the name of an enum.
+     */
+    private final String name;
 
 
+    /**
+     * Constructs a new {@link AccountStatus} with a specified name.
+     * @param s the specified name
+     */
+    AccountStatus(String s) {
+        name = s;
+    }
+
+    @Override
+    public String toString() {
+        return name;
+    }
 }
